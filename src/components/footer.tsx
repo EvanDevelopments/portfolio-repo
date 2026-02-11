28b44dcf61cdfa795d7327f65a04f05094372b8f
unknown <e.endless.yt@gmail.com>
2026-02-11 02:28:54 +0000

    new verion

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Footer({}) {
   return (
-    <nav className="flex justify-between items-center p-30 bg-[#0f0f0f] border-b border-zinc-800 text-white w-full mb-10">
+    <nav className="flex justify-between items-center p-40 bg-[#0f0f0f] border-b border-zinc-800 text-white w-full mb-10">
 
    
     </nav>
