b31e9ead14ea5bab5c25989954f889b4af28943b
unknown <e.endless.yt@gmail.com>
2026-02-20 23:23:07 +0000

    edited projects

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -16,16 +16,16 @@ export default function ContactPage() {
 
                     
                 <div className="mt-2.5 w-full max-w-2xl">
-                    <label htmlFor="email" className="block text-sm/6 font-semibold text-white-900">Email</label>
-                    <input id="email" name="email" type="email" autoComplete="email" className="block w-full rounded-md bg-zinc-900/50 px-3.5 py-2 text-base text-white-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-white-600"/>
+                    <label htmlFor="email" className="block text-sm/6 font-semibold text-white-900">Email<span className="text-red-400">*</span></label>
+                    <input id="email" name="email" placeholder="email@example.com" required type="email" autoComplete="email" className="block w-full rounded-md bg-zinc-900/50 px-3.5 py-2 text-base text-white-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-white-600"/>
                 </div>
 
                 <div className="w-full max-w-2xl">
-                    <label htmlFor="message" className="block text-sm/6 font-semibold text-white-900">Message</label>
-                    <textarea id="message" name="message" rows={4} defaultValue={""} className="block w-full rounded-md bg-zinc-900/50 px-3.5 py-2 text-base text-white-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-white-600 hover:border-white/20 transition-colors"/>
+                    <label htmlFor="message" className="block text-sm/6 font-semibold text-white-900">Message<span className="text-red-400">*</span></label>
+                    <textarea id="message" name="message" placeholder="Example message here..." required rows={4} defaultValue={""} className="block w-full rounded-md bg-zinc-900/50 px-3.5 py-2 text-base text-white-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-white-600 hover:border-white/20 transition-colors"/>
                 </div>
 
-                <button className="bg-white hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mt-4">
+                <button type="submit" className="bg-white hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mt-4">
                     <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"/></svg>
                     <span>Submit</span>
                 </button>
