a9a9a9e219314655edc1c2843082322434f1ad74
unknown <e.endless.yt@gmail.com>
2026-02-20 19:58:29 +0000

    edited projects

---
+++ src/app/contact/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'; 
+
+
+
+export default function Navbar({}) {
+  return (
+    <nav className="flex justify-between items-center p-6 bg-[#0f0f0f] border-b border-zinc-800 text-white w-full">
+      <div className="font-bold text-xl tracking-tight">EVAN DENHOLM-CHAPMAN</div>
+
+      <div className="flex gap-6 items-center">
+          <Link href="/" className="hover:text-slate-200 transition-colors">HOME</Link>
+          <Link href="/resume" className=" hover:bg-slate-200 hover:text-black text-white px-4 py-2 rounded-lg font-medium transition-all active:scale-95">RESUME</Link>
+          <Link href="/contact" className="bg-white hover:bg-slate-200 text-black px-4 py-2 rounded-lg font-medium transition-all active:scale-95">CONTACT ME</Link>
+      </div>
+   
+    </nav>
+  );
+}
